64b19f622820560e0df02aa2e0c46dd065917253
Wesley Darlington <wesley.darlington@gmail.com>
2024-03-15 06:25:01 -0400

    day 19

---
+++ day19a.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+
+example_input = """
+     |          
+     |  +--+    
+     A  |  C    
+ F---|----E|--+ 
+     |  |  |  D 
+     +B-+  +--+ 
+"""
+
+BACK = {'N': 'S', 'S': 'N', 'E': 'W', 'W': 'E'}
+LEFT = {'N': 'E', 'E': 'S', 'S': 'W', 'W': 'N'}
+RIGHT = {'N': 'W', 'W': 'S', 'S': 'E', 'E': 'N'}
+
+class Maze(object):
+    def __init__(self, inp):
+        self.g = inp.splitlines()
+    def start(self):
+        for i in range(len(self.g)):  # Blank first line?
+            if '|' in self.g[i]:
+                return (i, self.g[i].index('|'))
+    def at(self, p):
+        row, col = p
+        if row < 0 or len(self.g) <= row or col < 0 or len(self.g[row]) <= col:
+            return ' '
+        return self.g[row][col]
+    def move(self, p, v):
+        row, col = p
+        for w in [v, LEFT[v], RIGHT[v]]:
+            match w:
+                case 'N':
+                    q = (row-1, col)
+                case 'S':
+                    q = (row+1, col)
+                case 'E':
+                    q = (row, col+1)
+                case 'W':
+                    q = (row, col-1)
+            c = self.at(q)
+            if c != ' ':
+                return (q, w, c)
+        return None, None, None
+        
+def run(inp):
+    maze = Maze(inp)
+    p, v = maze.start(), 'S'
+    c = maze.at(p)
+    letters = []
+    while c:
+        if c.isalpha():
+            letters += c
+        p, v, c = maze.move(p, v)
+    return ''.join(letters)
+
+assert (got := run(example_input)) == 'ABCDEF', got
+
+with open('inputs/day19.input.txt') as f:
+    real_input = f.read()
+print(run(real_input)) # => VEBTPXCHLI
